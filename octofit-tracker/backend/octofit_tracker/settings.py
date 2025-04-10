d959b7d6310895968596541b9dda442e75f87c9a
Clairelilic <128017524+Clairelilic@users.noreply.github.com>
2025-04-10 01:12:35 +0000

    Fix compatibility issues and update dependencies for Django and pymongo

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-21g#q==ql5_hge=zjkkpbc4ccp$kc(s602_gwp=kb4wmehk7yp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[REPLACE-THIS-WITH-YOUR-CODESPACE-NAME]-8000.app.github.dev']
 
 
 # Application definition
